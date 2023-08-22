f6fe8daf3367918beefbbeb8bf59271513bf8eeb
saurabh mishra <saurabhmishra16121999@gmail.com>
2023-08-23 02:10:13 +0530

    Add query support to run for a duration

--- DotLoader/Workloads/QueryWorkload.cs
+++ DotLoader/Workloads/QueryWorkload.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Data.Common;
 using Couchbase;
 using Couchbase.KeyValue;
 using Couchbase.Management.Query;
@@ -23,27 +24,26 @@ class QueryWorkload
 
     private static string[] queries =
     {
-        "SELECT x.* FROM `{0}`.{1}.{2} x LIMIT 100;",
-        "select meta().id, firstName, lastName, email, age, address from `{0}`.{1}.{2} where age between 0 and 50 limit 100;",
-        "select firstName, lastName, attributes, address from `{0}`.{1}.{2} where attributes.hair.type=\"wavy\" limit 100;",
-        "select age, count(*) from `{0}`.{1}.{2} where maritalStatus='Single' group by age order by age limit 100;",
-        "select firstName, gender, address.city, hobby from `{0}`.{1}.{2} where gender=\"feminine\" limit 100;",
-        "select mishra.address.street, mishra.firstName, mishra.age, hobby from `{0}`.{1}.{2} unnest hobbies as hobby limit 100",
-        "select age, maritalStatus from `{0}`.{1}.{2} where hobbies is not null limit 100",
-        "select gender, count(*) from `{0}`.{1}.{2} group by gender order by gender limit 100;",
-        "select firstName, email, attributes from `{0}`.{1}.{2} where attributes.weight between 100 and 150 and attributes.height between 150 and 250 limit 100;",
+        "select x.* from `{0}`.`{1}`.`{2}` x limit 3000;",
+        "select meta().id, firstName, lastName, email, age, address from `{0}`.`{1}`.`{2}` where age between 0 and 50 limit 3000;",
+        "select firstName, lastName, attributes, address from `{0}`.`{1}`.`{2}` where attributes.hair.type=\"wavy\" limit 3000;",
+        "select age, count(*) from `{0}`.`{1}`.`{2}` where maritalStatus='Single' group by age order by age limit 3000;",
+        "select firstName, gender, address.city, hobby from `{0}`.`{1}`.`{2}` where gender=\"feminine\" limit 3000;",
+        "select age, maritalStatus from `{0}`.`{1}`.`{2}` where hobbies is not null limit 3000",
+        "select gender, count(*) from `{0}`.`{1}`.`{2}` group by gender order by gender limit 3000;",
+        "select firstName, email, attributes from `{0}`.`{1}`.`{2}` where attributes.weight between 100 and 150 and attributes.height between 150 and 250 limit 3000;",
     };
 
     private static string[] indexes =
     {
-        "CREATE PRIMARY INDEX ON {0}.{1}.{2};",
-        "CREATE INDEX ix_name ON `{0}`.{1}.{2}(firstName) WITH {\"defer_build\": true};",
-        "CREATE INDEX ix_email ON `{0}`.{1}.{2}(email) WITH {\"defer_build\": true};",
-        "CREATE INDEX ix_age_over_age on `{0}`.{1}.{2}(age) where age between 30 and 50 WITH {\"defer_build\": true};",
-        "CREATE INDEX ix_age_over_firstName on `{0}`.{1}.{2}(firstName) where age between 0 and 50 WITH {\"defer_build\": true};",
-        "CREATE INDEX ix_age_marital on ``{0}`.{1}.{2}(marital,age) USING GSI WITH {\"defer_build\": true};",
-        "CREATE INDEX ix_gender_address_city_hobby on `{0}`.{1}.{2}(Gender,address.city, DISTINCT ARRAY hobby FOR hobby in Hobbies END) where Gender=\"feminine\" WITH {\"defer_build\": true};",
-        "BUILD INDEX ON `{0}`.{1}.{2}(`#primary`, `ix_name`, `ix_email`, `ix_age_over_age`, `ix_age_over_firstName`, `ix_age_marital`, `ix_gender_address_city_hobby`)"
+        "CREATE PRIMARY INDEX ON `{0}`.`{1}`.`{2}`;",
+        "CREATE INDEX ix_name ON `{0}`.`{1}`.`{2}` (firstName);",
+        "CREATE INDEX ix_email ON `{0}`.`{1}`.`{2}` (email);",
+        "CREATE INDEX ix_age_over_age on `{0}`.`{1}`.`{2}`(age) where age between 30 and 50;",
+        "CREATE INDEX ix_age_over_firstName on `{0}`.`{1}`.`{2}`(firstName) where age between 0 and 50;",
+        "CREATE INDEX ix_age_marital on `{0}`.`{1}`.`{2}`(marital,age) USING GSI;",
+        "CREATE INDEX ix_gender_address_city_hobby on `{0}`.`{1}`.`{2}`(Gender,address.city, DISTINCT ARRAY hobby FOR hobby in Hobbies END) where Gender=\"feminine\";",
+        "BUILD INDEX ON `{0}`.`{1}`.`{2}`(`#primary`, `ix_name`, `ix_email`, `ix_age_over_age`, `ix_age_over_firstName`, `ix_age_marital`)"
     };
 
     public QueryWorkload(ICluster cluster, ICouchbaseCollection[] collections)
@@ -60,15 +60,16 @@ class QueryWorkload
         while (!cancellationToken.IsCancellationRequested)
         {
             var tasks = new List<Task>();
-            for (var i = startBatch; i < startBatch + batchSize; i++)
+            foreach (var collection in _collections)
             {
-                var collectionForOp = _collections[i % _collectionsLength];
-
-                var task = _cluster.QueryAsync<dynamic>(String.Format(queries[i % queries.Length],
-                    collectionForOp.Scope.Bucket.Name, collectionForOp.Scope.Name, collectionForOp.Name));
-                tasks.Add(task);
+                var queryLength = queries.Length;
+                for (var i = 0; i < queryLength; i++)
+                {
+                    var task = _cluster.QueryAsync<dynamic>(String.Format(queries[i],
+                        collection.Scope.Bucket.Name, collection.Scope.Name, collection.Name));
+                    tasks.Add(task);
+                }
             }
-
             try
             {
                 await Task.WhenAll(tasks);
@@ -95,24 +96,22 @@ class QueryWorkload
 
     public async Task BuildIndexes()
     {
-        // foreach (var idx in indexes)
-        // {
-            foreach (var collection in _collections)
+        foreach (var collection in _collections)
+        {
+            var indexLength = indexes.Length;
+            for (var i = 0; i < indexLength; i++)
             {
                 try
-                { 
-                    Console.WriteLine("{0}.{1}.{2}",
-                        collection.Scope.Bucket.Name, collection.Scope.Name, collection.Name); 
-                    await  collection.QueryIndexes.CreatePrimaryIndexAsync(CreatePrimaryQueryIndexOptions.Default);
-                    // await _cluster.QueryAsync<dynamic>(String.Format(idx,
-                    //     collection.Scope.Bucket.Name, collection.Scope.Name, collection.Name));
+                {
+                    await _cluster.QueryAsync<dynamic>(String.Format(indexes[i],
+                        collection.Scope.Bucket.Name, collection.Scope.Name, collection.Name));
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Got error building indexes: " + e.Message);
                 }
             }
-        // }
+        }
     }
     
     public async Task PrintResultsPeriodically(CancellationToken cancellationToken)
